a15cf14d16c233768c3f23acc0a3add3f32ada35
delver <dima.plugaturevchik@gmail.com>
2024-08-16 19:42:50 +0300

    improved naming, made optimizations

---
+++ GnomeSort/Sorters/ParallelGnomeSorter.cs
@@ -0,0 +1,100 @@
+﻿namespace GnomeSort.Sorters;
+
+/// <summary>
+/// Provides a method to perform parallel Gnome Sort on an array of integers.
+/// </summary>
+public class ParallelGnomeSorter
+{
+    /// <summary>
+    /// Sorts the specified array using the parallel Gnome Sort algorithm.
+    /// </summary>
+    /// <param name="inputArray">The array of integers to sort.</param>
+    /// <param name="numberOfThreads">The number of threads to use for sorting.</param>
+    /// <returns>A new array containing the sorted integers.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the input array is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the number of threads is less than or equal to zero.</exception>
+    public int[] Sort(int[] inputArray, int numberOfThreads)
+    {
+        if (numberOfThreads <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numberOfThreads),
+                "Number of threads must be greater than zero.");
+        }
+
+        if (inputArray.Length < numberOfThreads)
+        {
+            numberOfThreads = inputArray.Length;
+        }
+        
+        var segmentLength = inputArray.Length / numberOfThreads;
+        var sortingTasks = new Task[numberOfThreads];
+        var sortedArray = (int[])inputArray.Clone();
+        var sequentialSorter = new SequentialGnomeSorter();
+
+        for (var i = 0; i < numberOfThreads; i++)
+        {
+            var segmentStartIndex = i * segmentLength;
+            var segmentEndIndex = i == numberOfThreads - 1 
+                ? sortedArray.Length : segmentStartIndex + segmentLength;
+            var localArray = sortedArray;
+
+            sortingTasks[i] = Task.Run(() =>
+            {
+                var sortedSegment = sequentialSorter.Sort(localArray, segmentStartIndex, segmentEndIndex);
+                Array.Copy(sortedSegment, segmentStartIndex, 
+                    localArray, segmentStartIndex, segmentEndIndex - segmentStartIndex);
+            });
+        }
+
+        Task.WaitAll(sortingTasks);
+        sortedArray = MergeSegments(sortedArray, numberOfThreads, segmentLength);
+        return sortedArray;
+    }
+
+    /// <summary>
+    /// Merges the sorted segments of the array.
+    /// </summary>
+    /// <param name="array">The array containing sorted segments.</param>
+    /// <param name="segmentCount">The number of segments.</param>
+    /// <param name="segmentSize">The size of each segment.</param>
+    /// <returns>A new array containing the merged sorted segments.</returns>
+    private int[] MergeSegments(int[] array, int segmentCount, int segmentSize)
+    {
+        var mergedArray = (int[])array.Clone();
+    
+        for (var segmentIndex = 1; segmentIndex < segmentCount; segmentIndex++)
+        {
+            var currentSegmentStart = segmentIndex * segmentSize;
+            var currentSegmentEnd = segmentIndex == segmentCount - 1 
+                ? array.Length : currentSegmentStart + segmentSize;
+            
+            var tempMergedSegment = new int[currentSegmentEnd];
+            var mergedSegmentIndex = 0;
+            var previousSegmentIndex = 0;
+            var currentSegmentPointer = currentSegmentStart;
+    
+            while (previousSegmentIndex < currentSegmentStart 
+                   && currentSegmentPointer < currentSegmentEnd)
+            {
+                tempMergedSegment[mergedSegmentIndex++] 
+                    = mergedArray[previousSegmentIndex] <= mergedArray[currentSegmentPointer] 
+                    ? mergedArray[previousSegmentIndex++] : mergedArray[currentSegmentPointer++];
+            }
+    
+            while (previousSegmentIndex < currentSegmentStart)
+            {
+                tempMergedSegment[mergedSegmentIndex++] = mergedArray[previousSegmentIndex++];
+            }
+    
+            while (currentSegmentPointer < currentSegmentEnd)
+            {
+                tempMergedSegment[mergedSegmentIndex++] = mergedArray[currentSegmentPointer++];
+            }
+    
+            Array.Copy(tempMergedSegment, 0, mergedArray, 
+                0, currentSegmentEnd);
+        }
+    
+        return mergedArray;
+    }
+}
---
+++ GnomeSort/Sorters/SequentialGnomeSorter.cs
@@ -0,0 +1,77 @@
+﻿namespace GnomeSort.Sorters;
+
+/// <summary>
+/// Provides a method to perform Gnome Sort on an array of integers.
+/// </summary>
+public class SequentialGnomeSorter
+{
+    /// <summary>
+    /// Sorts a segment of the specified array using the Gnome Sort algorithm.
+    /// </summary>
+    /// <param name="array">The array of integers to sort.</param>
+    /// <param name="segmentStart">The starting index of the segment to sort.</param>
+    /// <param name="segmentEnd">The ending index of the segment to sort.</param>
+    /// <returns>A new array containing the sorted segment.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the input array is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the start or end index is out of range.</exception>
+    /// <exception cref="ArgumentException">Thrown when the start index is not less than the end index.</exception>
+    public int[] Sort(int[] array, int segmentStart, int segmentEnd)
+    {
+        ValidateParameters(array, segmentStart, segmentEnd);
+        var currentIndex = segmentStart;
+        var sortedSegment = (int[])array.Clone();
+
+        while (currentIndex < segmentEnd)
+        {
+            if (currentIndex == segmentStart || sortedSegment[currentIndex] >= sortedSegment[currentIndex - 1])
+            {
+                currentIndex++;
+            }
+            else
+            {
+                (sortedSegment[currentIndex], sortedSegment[currentIndex - 1]) 
+                    = (sortedSegment[currentIndex - 1], sortedSegment[currentIndex]);
+                currentIndex--;
+            }
+        }
+
+        return sortedSegment;
+    }
+
+    /// <summary>
+    /// Sorts the specified array using the Gnome Sort algorithm.
+    /// </summary>
+    /// <param name="array">The array of integers to sort.</param>
+    /// <returns>A new array containing the sorted integers.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the input array is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the start or end index is out of range.</exception>
+    /// <exception cref="ArgumentException">Thrown when the start index is not less than the end index.</exception>
+    public int[] Sort(int[] array)
+    {
+        return Sort(array, 0, array.Length);
+    }
+    
+    private void ValidateParameters(int[] array, int segmentStart, int segmentEnd)
+    {
+        if (array == null)
+        {
+            throw new ArgumentNullException(nameof(array), "Input array cannot be null.");
+        }
+
+        if (segmentStart < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(segmentStart), "Start index cannot be negative.");
+        }
+
+        if (segmentEnd > array.Length)
+        {
+            throw new ArgumentOutOfRangeException(nameof(segmentEnd), 
+                "End index cannot be greater than the array length.");
+        }
+
+        if (segmentStart >= segmentEnd)
+        {
+            throw new ArgumentException("Start index must be less than end index.", nameof(segmentStart));
+        }
+    }
+}
