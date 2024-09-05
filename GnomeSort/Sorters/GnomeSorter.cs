9bbeb9d2d5352538f1b053f00f46a37f9ef980f9
delver <dima.plugaturevchik@gmail.com>
2024-09-05 14:10:15 +0300

    fixed incorrectly working sequential sorter

---
+++ GnomeSort/Sorters/GnomeSorter.cs
@@ -0,0 +1,94 @@
+﻿namespace GnomeSort.Sorters;
+
+public abstract class GnomeSorter<T> where T : IComparable<T>
+{
+    protected void GnomeSort(T[] array)
+    {
+        GnomeSort(array, 0, array.Length - 1);
+    }
+    
+    protected void GnomeSort(T[] array, int startIndex, int endIndex)
+    {
+        var currentIndex = startIndex + 1;
+
+        while (currentIndex <= endIndex)
+        {
+            if (currentIndex == startIndex
+                || array[currentIndex].CompareTo(array[currentIndex - 1]) >= 0)
+            {
+                currentIndex++;
+            }
+            else
+            {
+                Swap(array, currentIndex, currentIndex - 1);
+                currentIndex--;
+            }
+        }
+    }
+
+    protected T[] MergeSegments(ArraySegment<T>[] segments)
+    {
+        var result = new T[segments.Sum(s => s.Count)];
+        var indices = new int[segments.Length]; 
+
+        var resultIndex = 0;
+        T minValue = default;
+
+        while (true)
+        {
+            var minSegmentIndex = -1;
+
+            for (var i = 0; i < segments.Length; i++)
+            {
+                if (indices[i] >= segments[i].Count)
+                {
+                    continue;
+                }
+
+                if (minSegmentIndex != -1 && 
+                    segments[i].Array![segments[i].Offset + indices[i]].CompareTo(minValue) >= 0)
+                {
+                    continue;
+                }
+
+                minValue = segments[i].Array![segments[i].Offset + indices[i]];
+                minSegmentIndex = i;
+            }
+
+            if (minSegmentIndex == -1)
+            {
+                break;
+            }
+
+            result[resultIndex++] = minValue;
+            indices[minSegmentIndex]++;
+        }
+
+        return result;
+    }
+
+    protected int CalculateOptimalSegmentSize(T[] array)
+    {
+        const int minimumSegmentSize = 64;
+
+        if (array.Length <= minimumSegmentSize)
+        {
+            return array.Length;
+        }
+
+        var baseSegmentSize = array.Length switch
+        { 
+            <= 1000 => Math.Min(minimumSegmentSize, array.Length / 8),     
+            <= 100000 => Math.Max(minimumSegmentSize, array.Length / 500), 
+            <= 1000000 => Math.Max(minimumSegmentSize, array.Length / 1300),
+            _ => Math.Max(minimumSegmentSize, array.Length / 2000)         
+        };
+
+        return baseSegmentSize;
+    }
+
+    protected void Swap(T[] array, int index1, int index2)
+    {
+        (array[index1], array[index2]) = (array[index2], array[index1]);
+    }
+}
--- GnomeSort/Sorters/SequentialHybridGnomeMergeSorter.cs
+++ GnomeSort/Sorters/SequentialHybridGnomeMergeSorter.cs
@@ -1,124 +1,35 @@
-﻿using System.Buffers;
+﻿namespace GnomeSort.Sorters;
 
-namespace GnomeSort.Sorters;
-
-public class SequentialHybridGnomeMergeSorter<T> where T : IComparable<T>
+public class SequentialHybridGnomeMergeSorter<T> : GnomeSorter<T> where T : IComparable<T>
 {
     private int _segmentSize;
-    private readonly ArrayPool<T> _arrayPool = ArrayPool<T>.Shared;
     
     public T[] Sort(T[] array)
     {
         ValidateParameters(array);
         _segmentSize = CalculateOptimalSegmentSize(array);
         var numberOfSegments = (int)Math.Ceiling((double)array.Length / _segmentSize);
-        var segments = new T[numberOfSegments][];
+        
+        var segmentData = new T[array.Length];
+        var segments = new ArraySegment<T>[numberOfSegments];
 
         for (var i = 0; i < numberOfSegments; i++)
         {
             var start = i * _segmentSize;
             var end = Math.Min(start + _segmentSize, array.Length);
-            segments[i] = _arrayPool.Rent(end - start); 
-            Array.Copy(array, start, segments[i], 0, end - start);
-        }
         
+            segments[i] = new ArraySegment<T>(segmentData, start, end - start); 
+            Array.Copy(array, start, segmentData, start, end - start); 
+        }
+
         foreach (var segment in segments)
         {
-            GnomeSort(segment);
+            GnomeSort(segment.Array, segment.Offset, segment.Offset + segment.Count - 1); 
         }
-        
+
         var result = MergeSegments(segments);
-        
-        foreach (var segment in segments)
-        {
-            _arrayPool.Return(segment);
-        }
 
         return result;
-    }
-    
-    private void GnomeSort(T[] array)
-    {
-        var currentIndex = 1;
-
-        while (currentIndex < array.Length)
-        {
-            if (currentIndex == 0
-                || array[currentIndex].CompareTo(array[currentIndex - 1]) >= 0)
-            {
-                currentIndex++;
-            }
-            else
-            {
-                Swap(array, currentIndex, currentIndex - 1);
-                currentIndex--;
-            }
-        }
-    }
-
-    private T[] MergeSegments(T[][] segments)
-    {
-        var result = new T[segments.Sum(s => s.Length)];
-        var indices = new int[segments.Length];
-
-        var resultIndex = 0;
-        
-        while (true)
-        {
-            T minValue = default;
-            var minSegmentIndex = -1;
-
-            for (var i = 0; i < segments.Length; i++)
-            {
-                if (indices[i] >= segments[i].Length)
-                {
-                    continue;
-                }
-                
-                if (minSegmentIndex != -1 && segments[i][indices[i]].CompareTo(minValue) >= 0)
-                {
-                    continue;
-                }
-                
-                minValue = segments[i][indices[i]];
-                minSegmentIndex = i;
-            }
-
-            if (minSegmentIndex == -1)
-            {
-                break;
-            }
-
-            result[resultIndex++] = minValue;
-            indices[minSegmentIndex]++;
-        }
-
-        return result;
-    }
-
-    private int CalculateOptimalSegmentSize(T[] array)
-    {
-        const int minimumSegmentSize = 64;
-
-        if (array.Length <= minimumSegmentSize)
-        {
-            return array.Length;
-        }
-
-        var baseSegmentSize = array.Length switch
-        { 
-            <= 1000 => Math.Min(minimumSegmentSize, array.Length / 8),     
-            <= 100000 => Math.Max(minimumSegmentSize, array.Length / 500), 
-            <= 1000000 => Math.Max(minimumSegmentSize, array.Length / 1300),
-            _ => Math.Max(minimumSegmentSize, array.Length / 2000)         
-        };
-
-        return baseSegmentSize;
-    }
-
-    private void Swap(T[] array, int index1, int index2)
-    {
-        (array[index1], array[index2]) = (array[index2], array[index1]);
     }
 
     private void ValidateParameters(T[] array)
@@ -132,6 +43,5 @@ public class SequentialHybridGnomeMergeSorter<T> where T : IComparable<T>
         {
             throw new ArgumentOutOfRangeException(nameof(array), "Input array cannot be empty.");
         }
-        
     }
 }
